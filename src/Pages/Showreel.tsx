1a279ea9978f7b294d608b97e50d9593468d3d76
Mattias Gulec <Voltair88@gmail.com>
2022-07-06 09:40:39 +0200

    updated showreel

--- src/Pages/Showreel.tsx
+++ src/Pages/Showreel.tsx
@@ -63,6 +63,55 @@ export default function Showreel() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
+      <iframe
+        className="skeleton"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/O5KAKB6f-DU"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/MRkMOrh2LBI"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        className="skeleton"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/lBwA_yJwxho"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        className="skeleton"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/4IBbn61yC-c"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        className="skeleton"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/7ABe1fQhzFw"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </Show>
   );
 }
