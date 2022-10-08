fa951198171965e74d934ed692c976fddc4327c7
Mattias Gulec <Voltair88@gmail.com>
2022-10-08 15:20:21 +0200

    updated the CV

--- src/Pages/Showreel.tsx
+++ src/Pages/Showreel.tsx
@@ -4,6 +4,7 @@ export default function Showreel() {
   return (
     <Show>
       <Player src="https://www.youtube.com/embed/GSzP953UJlM" />
+      <Player src="https://www.youtube.com/embed/05XFB2ZOESg" />
       <Player src="https://www.youtube.com/embed/GpplYQPzSxc" />
       <Player src="https://www.youtube.com/embed/RzCpF4VSQBo" />
       <Player src="https://www.youtube.com/embed/rLLD3WmikJs" />
@@ -13,6 +14,27 @@ export default function Showreel() {
       <Player src="https://www.youtube.com/embed/lBwA_yJwxho" />
       <Player src="https://www.youtube.com/embed/4IBbn61yC-c" />
       <Player src="https://www.youtube.com/embed/7ABe1fQhzFw" />
+      <Player src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1508522236&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></Player>
+      <div>
+        <a
+          href="https://soundcloud.com/mattias-gulec"
+          title="Mattias Gulec"
+          target="_blank"
+          style={{ color: 'white', textDecoration: 'none' }}
+          rel="noreferrer"
+        >
+          Mattias Gulec
+        </a>
+        <a
+          href="https://soundcloud.com/mattias-gulec/sets/piotr-giro-laser-upp-sjalvportratt-av-okanda-man"
+          title='Piotr Giro läser upp "Självporträtt av okända män"'
+          target="_blank"
+          style={{ color: 'white', textDecoration: 'none' }}
+          rel="noreferrer"
+        >
+          Piotr Giro läser upp &quot;Självporträtt av okända män&quot;
+        </a>
+      </div>
     </Show>
   );
 }
